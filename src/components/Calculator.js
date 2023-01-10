4a32e6bfa6aa61ab9067d6011c28c0151a70e372
ndaboom <ndabosam084@gmail.com>
2023-01-10 08:48:31 +0100

    Calculator UI implemented

---
+++ src/components/Calculator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <h1>{ this.props.name }</h1>
+        <div className="screen-container">0</div>
+        <div className="touch-pad">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button className="orange" type="button">÷</button>
+
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button className="orange" type="button">x</button>
+
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button className="orange" type="button">-</button>
+
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button className="orange" type="button">+</button>
+
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button className="orange" type="button">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
