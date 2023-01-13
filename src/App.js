768c2af3b7474ff2a932d16ad25eafb7d5a64f4c
ndaboom <ndabosam084@gmail.com>
2023-01-13 11:50:40 +0100

    Calculator logic added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/prefer-stateless-function */
 import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="main-container">
-        <Calculator name="Calculator 1.0" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="main-container">
+      <Calculator name="Calculator 1.0" />
+    </div>
+  );
 }
-
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,60 @@
-/* eslint-disable react/no-unused-state */
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function Calculator(props) {
+  const [state, setState] = useState({
+    total: 0,
+    next: 0,
+    operation: '',
+  });
 
-  render() {
-    return (
-      <div className="calculator">
-        <h1>{ this.props.name }</h1>
-        <div className="screen-container">0</div>
-        <div className="touch-pad">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="orange" type="button">÷</button>
+  const clickHandler = (e) => {
+    setState(calculate(state, e.target.textContent));
+  };
 
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="orange" type="button">x</button>
+  const { next, total } = state;
 
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="orange" type="button">-</button>
+  return (
+    <div className="calculator">
+      <h1>{ props.name }</h1>
+      <div className="screen-container">{next || total || 0}</div>
+      <div className="touch-pad">
+        <button onClick={clickHandler} type="button">AC</button>
+        <button onClick={clickHandler} type="button">+/-</button>
+        <button onClick={clickHandler} type="button">%</button>
+        <button onClick={clickHandler} className="orange" type="button">÷</button>
 
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="orange" type="button">+</button>
+        <button onClick={clickHandler} type="button">7</button>
+        <button onClick={clickHandler} type="button">8</button>
+        <button onClick={clickHandler} type="button">9</button>
+        <button onClick={clickHandler} className="orange" type="button">x</button>
 
-          <button type="button">0</button>
-          <button type="button">.</button>
-          <button className="orange" type="button">=</button>
-        </div>
+        <button onClick={clickHandler} type="button">4</button>
+        <button onClick={clickHandler} type="button">5</button>
+        <button onClick={clickHandler} type="button">6</button>
+        <button onClick={clickHandler} className="orange" type="button">-</button>
+
+        <button onClick={clickHandler} type="button">1</button>
+        <button onClick={clickHandler} type="button">2</button>
+        <button onClick={clickHandler} type="button">3</button>
+        <button onClick={clickHandler} className="orange" type="button">+</button>
+
+        <button onClick={clickHandler} type="button">0</button>
+        <button onClick={clickHandler} type="button">.</button>
+        <button onClick={clickHandler} className="orange" type="button">=</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Calculator.propTypes = {
+  name: PropTypes.string,
+};
+
+Calculator.defaultProps = {
+  name: 'Calculator 1.0',
+};
 
 export default Calculator;
